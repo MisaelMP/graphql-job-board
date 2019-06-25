ed3fcf93e0db6c41708134117d14f70d8653f10b
Misael Mercado <mme@codehousegroup.com>
2019-06-25 12:59:15 +1000

    addec schema for companies and resolver function

--- server/server.js
+++ server/server.js
@@ -1,16 +1,18 @@
-const { graphqlExpress, graphqilExpress } = require ('apollo-server-express');
+const { graphqlExpress, graphiqlExpress } = require ('apollo-server-express');
 const { makeExecutableSchema } = require('graphql-tools')
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const express = require('express');
 const expressJwt = require('express-jwt');
+const fs = require('fs');
 const jwt = require('jsonwebtoken');
 const db = require('./db');
 
 const port = 9000;
 const jwtSecret = Buffer.from('Zn8Q5tyZ/G1MHltc4F/gTkVJMlrbKiZt', 'base64');
-const typeDefs = '';
-const resolvers = {};
+
+const typeDefs = fs.readFileSync('./schema.graphql', {encoding: 'utf-8'});
+const resolvers = require('./resolvers');
 const schema = makeExecutableSchema({typeDefs, resolvers});
 
 const app = express();
@@ -19,8 +21,8 @@ app.use(cors(), bodyParser.json(), expressJwt({
   credentialsRequired: false
 }));
 
-app.use('/graphl', graphqlExpress({schema}));
-app.use('/graphiql', graphqilExpress({endpointURL: '/graphql'}));
+app.use('/graphql', graphqlExpress({schema}));
+app.use('/graphiql', graphiqlExpress({endpointURL: '/graphql'}));
 
 app.post('/login', (req, res) => {
   const {email, password} = req.body;
